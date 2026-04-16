1d3aa7c9ed2d9ae187f2913dabeb573c2726bc1f
AlexandtheSackboys <a.j.costin1@gmail.com>
2026-04-16 18:30:22 +0100

    setup: FirstPersonController class C++

---
+++ Source/CultLand2/CultLand2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CultLand2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CultLand2, "CultLand2" );
