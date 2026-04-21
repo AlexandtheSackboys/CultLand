5dcd7a74cbc9034d584411683988939e8e953706
Warge09 <charlie10avfc@icloud.com>
2026-04-21 18:40:30 +0100

    Created some Weapon Data which each weapon can derive from and use

---
+++ Source/CultLand2/UWeaponData.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+// ----------------------------------------------------------	
+// Script (Header and Source files) Name: [UWeaponData]
+// 
+// Author: [Charlie Wargent]
+// 
+// Editors: []
+// 
+// Description: [Weapon data that other classes can use to configure weapon behavior]
+// 
+// creation Date: [21/4/2026]
+// 
+// last edited: [25/03/26 by Charlie Wagrnet]
+// 
+// Editors Contributions: []
+// ----------------------------------------------------------	
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "UWeaponData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CULTLAND2_API UUWeaponData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	float FireRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	float Recoil;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	float ReloadTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Ammo")
+	int32 MagazineSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Ammo")
+	int32 MaxAmmoCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Juice")
+	TArray<USoundBase*> ReloadSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Juice")
+	TArray<UParticleSystem*> WeaponEffect;
+
+};
