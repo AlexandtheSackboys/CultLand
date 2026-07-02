864ddd9d4b85a12e72b52ae092b02def62f9da65
itz-c9 <callumhadley3@gmail.com>
2026-07-02 11:54:17 +0100

    Fixed VisualStudio solution

---
+++ Source/CultLand2/Private/solutionTest.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "solutionTest.h"
+
+solutionTest::solutionTest()
+{
+}
+
+solutionTest::~solutionTest()
+{
+}
---
+++ Source/CultLand2/Public/solutionTest.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class CULTLAND2_API solutionTest
+{
+public:
+	solutionTest();
+	~solutionTest();
+};
