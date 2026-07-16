5e764366cf76f1198a896712eeb1f54b0b38e98a
itz-c9 <callumhadley3@gmail.com>
2026-07-16 15:53:34 +0100

    Revert "Created flare"
    This reverts commit 8e616f4afa69a279009866337667aa38bc7de3df.

--- Source/CultLand2/CultLand2.Build.cs
+++ Source/CultLand2/CultLand2.Build.cs
@@ -8,7 +8,7 @@ public class CultLand2 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" ,"EnhancedInput, Niagara", "NiagaraCore"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" ,"EnhancedInput"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
